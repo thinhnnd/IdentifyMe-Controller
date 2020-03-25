30fb3f320aebde24599395161f58683937c915f3
thupx <xuanthu404@gmail.com>
2020-03-25 11:56:26 +0700

    refactor(seed): generate random seed and refactor

--- src/controllers/uit.controller.ts
+++ src/controllers/uit.controller.ts
@@ -1,27 +1,41 @@
 import { Request, Response, Router } from 'express';
 import { IBaseController } from '../interface';
-import { UITAgentService } from '../services/uit.service';
+import { UITAgentService } from '../services/UIT.service';
+import { AGENT_PORT, ADMIN_PORT } from '../constant';
 export class UITController implements IBaseController {
     public path = '/';
     public router = Router();
-    private agentService: UITAgentService = new UITAgentService(3010, 5000, false);
+    private agentService: UITAgentService = new UITAgentService(AGENT_PORT, ADMIN_PORT, false);
     constructor() {
         this.initRoutes();
         this.agentService.bootstrap();
     }
     public initRoutes() {
         this.router.get('/', (req: Request, res: Response) => {
-            res.send('Hello from UIT Agent');
+            res.send(`<h1> This agent is ${this.agentService.agentName}</h1>`);
         });
-        this.registerSchema();
+        // this.registerSchema();
+        // this.createCredentialsDefinition();
+        // this.getCredDef();
     }
-    private registerSchema() {
-        this.router.post('/schemas', (req: Request, res: Response) => this.agentService.agentRegisterSchema(req, res));
-    }
-    private createCredentialsDefinition(){
-        this.router.post('/crea')
-    }
-    // private async getAllSchemas() {
-    //     return await this.agentService.getAllSchemas();
+    // private registerSchema() {
+    //     this.router.post('/schemas', (req: Request, res: Response) => this.agentService.agentRegisterSchema(req, res));
+    // }
+    // private createCredentialsDefinition() {
+    //     this.router.post('/create-cred-def', async (req: Request, res: Response) => {
+    //         const result = await this.agentService.createCredentialsDefinition(req.body);
+    //         res.json(result.data.credential_definition_id);
+    //     });
+    // }
+    // private getCredDef() {
+    //     this.router.get('cred-def', async (req: Request, res: Response) => {
+    //         const result = await this.agentService.getCredDef(req.params["credential_definition_id"]);
+    //         res.json(result.data.credential_definition);
+    //     });
+    // }
+    // private getAllSchemas() {
+    //     this.router.get('/schemas', async (req: Request, res: Response) => {
+    //         const result = await this.agentService.getAllSchemas(req.body)
+    //     })
     // }
 }
--- src/services/uit.service.ts
+++ src/services/uit.service.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
-import { BaseAgentService } from '../services/agent';
+import { BaseAgentService } from './agent';
 import { RegisterSchemasDTO } from '../dtos';
 import { getGenesisTxns } from '../utils';
-import { SEED } from '../constant';
+import { SEED, AGENT_MODULE } from '../constant';
 import { SchemaSendRequest } from 'src/interface/api';
+import { generate } from 'randomstring';
 export class UITAgentService extends BaseAgentService {
     public credAttrs: string[]
     constructor(httpPort: number | string, adminPort: number | string, noAuto: Boolean) {
@@ -11,19 +12,20 @@ export class UITAgentService extends BaseAgentService {
          * @description used for arguments in aca-py cli, read more by typing: aca-py start -h
          */
         const extractArgs = !noAuto ? [] : ["--auto-accept-invites", "--auto-accept-requests"];
+        const seed = generate({ length: 32, charset: "alphabetic" });
         super({
-            agentName: 'UIT_Agent',
+            agentName: AGENT_MODULE,
             httpPort: httpPort,
             adminPort: adminPort,
             args: extractArgs,
-            seed: SEED
+            seed: seed
         });
         // console.log(this)
         this.connectionId = '';
         this.credAttrs = [];
     }
     async bootstrap() {
-        console.log('bootstraping');
+        console.log('Bootstraping agent ', this.agentName);
         try {
             const genesis = await getGenesisTxns();
             this.genesisData = genesis;
@@ -38,19 +40,5 @@ export class UITAgentService extends BaseAgentService {
         } catch (error) {
             console.error(error);
         }
-    }
-    async agentRegisterSchema(req: Request, res: Response) {
-        try {
-            const body: SchemaSendRequest = req.body;
-            const result = await this.registerSchema(body);
-            console.log('result', result);
-            if (result) {
-                res.json(result);
-            }
-            else throw { error: 'Server response with empty body' };
-        } catch (error) {
-            res.json(error)
-        }
-
     }
 }
