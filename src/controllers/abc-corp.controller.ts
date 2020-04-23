1cea55d1f67512826248bd2037fa2e76e33a39e4
thupx <xuanthu404@gmail.com>
2020-04-23 11:09:07 +0700

    feat(agent): add new agent (ABC-Corp)

--- src/controllers/abc-corp.controller.ts
+++ src/controllers/abc-corp.controller.ts
@@ -1,7 +1,25 @@
 import { Request, Response, Router } from 'express';
-import { IBaseController } from '../interface';
+import {
+    IBaseController,
+    ConnectionInvitationQuery,
+    CredentialDefinitionsCreatedParams,
+    FilterSchema,
+    SendProofRequestPayload
+} from '../interface';
 import { ABCCorpAgentService } from '../services/ABC-corp.service';
 import { AGENT_PORT, ADMIN_PORT } from '../constant';
+import {
+    SchemaSendRequest,
+    V10CredentialOfferRequest,
+    CredentialPreview,
+    CredentialDefinitionGetResults,
+    CredentialDefinitionsCreatedResults,
+    SchemasCreatedResults,
+    SchemaGetResults,
+    IndyProofReqAttrSpec,
+    IndyProofReqPredSpec
+}
+    from 'src/interface/api';
 export class ABCCorpController implements IBaseController {
     public path = '/';
     public router = Router();
@@ -14,5 +32,240 @@ export class ABCCorpController implements IBaseController {
         this.router.get('/', (req: Request, res: Response) => {
             res.send(`<h1> This agent is ${this.agentService.agentName}</h1>`);
         });
+        this.createConnInvitationRoute();
+        this.createSchemasRoute();
+        this.issueCredentialRoute();
+        this.getConnections();
+        this.getSchemaById();
+        this.getAllSchemas();
+        this.findSchemas();
+
+        this.getAllCredentialDefinitions();
+        this.getCredentialDefinitionsByFilter();
+        this.getCredentialDefinitionsById();
+
+        //Proof
+        this.sendProofRequest();
     }
+    /**
+     * @description Create a new connection invitation and set it into current connection via connectionId.
+     */
+    private async createConnInvitationRoute() {
+        this.router.post('/connections', async (req: Request, res: Response) => {
+            try {
+                const query: ConnectionInvitationQuery = {
+                    alias: req.body.alias,
+                    accept: req.body.accept,
+                    multi_use: req.body.multi_use,
+                    public: req.body.public,
+                }
+                const result = await this.agentService.createConnectionInvitation(query);
+                console.log(`result: ${result}`);
+                res.json(result);
+            } catch (error) {
+                res.json(error);
+            }
+        })
+    }
+    private async issueCredentialRoute() {
+        this.router.post('/issue-credential/send-offer', async (req, res) => {
+            try {
+                const credentialPreview: CredentialPreview = {
+                    "@type": "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/issue-credential/1.0/credential-preview",
+                    attributes: req.body.attributes
+                }
+                const offer: V10CredentialOfferRequest = {
+                    connection_id: req.body.connection_id,
+                    credential_preview: credentialPreview,
+                    auto_issue: req.body.auto_issue,
+                    cred_def_id: req.body.cred_def_id,
+                    comment: req.body.comment,
+                    auto_remove: req.body.auto_remove,
+                    revoc_reg_id: req.body.revoc_reg_id,
+                }
+                const result = await this.agentService.issuerSendOffer(offer);
+                console.log(result);
+                res.json(result);
+            } catch (error) {
+                console.log(error);
+                res.json(error);
+            }
+        })
+    }
+    private async getConnections() {
+        this.router.get('/connections', async (req, res) => {
+            const connections = await this.agentService.getConnections();
+            res.json(connections);
+        })
+    }
+    private async createSchemasRoute() {
+        this.router.post('/schemas', async (req, res) => {
+            try {
+                const body: SchemaSendRequest = {
+                    attributes: req.body.attributes,
+                    schema_name: req.body.schema_name,
+                    schema_version: req.body.schema_version,
+                }
+                const result = await this.agentService.registerSchemaAndCredDef(body);
+                console.log("UITController -> createSchemas -> result", { ...result });
+                res.json({ ...result.data, ...result.credDefforSchema });
+            } catch (error) {
+                console.log(error);
+                res.json(error);
+            }
+        })
+    }
+    private async getSchemaById() {
+        this.router.get("/schema/:schema_id", async (req, res) => {
+            const schemaId = req.params.schema_id;
+            try {
+                const schema = await this.agentService.getSchema(schemaId);
+                const crefDef = await this.agentService.getSpecialCredDef({ schema_id: schemaId });
+                const result = {
+                    schema_json: { ...schema.schema_json, credential_definition_id: crefDef.credential_definition_ids[0] }
+                }
+                res.json(result);
+            } catch (error) {
+                console.log(error);
+                res.json(error);
+            }
+        });
+    }
+    private async getAllSchemas() {
+        this.router.get("/schemas", async (req, res) => {
+            try {
+                const schema = await this.agentService.getAllSchemas({ schema_issuer_did: this.agentService.did });
+                //Get the credential_definition_id for each schema
+                // const result = await Promise.all(schema.schema_ids.map(async schemaId => {
+                //     const credDef = await this.agentService.getSpecialCredDef({ schema_id: schemaId });
+                //     return {
+                //         schema_id: schemaId,
+                //         credential_definition_ids: credDef.credential_definition_ids[0]
+                //     }
+                // }));
+                res.json(schema);
+            } catch (error) {
+                console.log(error);
+                res.json(error);
+            }
+        });
+    }
+    private async findSchemas() {
+        this.router.get("/schemas", async (req, res) => {
+            try {
+                const filter: FilterSchema = {
+                    schema_id: req.body.schema_id,
+                    schema_issuer_did: req.body.schema_issuer_did,
+                    schema_name: req.body.schema_name,
+                    schema_version: req.body.schema_version,
+                }
+                const schema = await this.agentService.getAllSchemas(filter);
+                res.json(schema);
+            } catch (error) {
+                console.log(error);
+                res.json(error);
+            }
+        });
+    }
+    private async getAllCredentialDefinitions() {
+        this.router.get('/credential-definitions', async (req, res) => {
+            try {
+                const result = await this.agentService.getAllCredentialDefinitions();
+                res.json(result);
+            } catch (error) {
+                console.log(error);
+                res.json(error);
+            }
+        })
+    }
+    private async getCredentialDefinitionsByFilter() {
+        this.router.get('/credential-definitions-filter', async (req, res) => {
+            const params: CredentialDefinitionsCreatedParams = {
+                schema_id: req.query.schema_id,
+                schema_issuer_did: req.query.schema_issuer_did,
+                schema_name: req.query.schema_name,
+                schema_version: req.query.schema_version,
+                issuer_did: req.query.issuer_did,
+                cred_def_id: req.query.cred_def_id,
+            };
+            try {
+                const result = await this.agentService.getSpecialCredDef(params);
+                res.json(result);
+            } catch (error) {
+                console.log(error);
+                res.json(error);
+            }
+        })
+    }
+    private async getCredentialDefinitionsById() {
+        this.router.get('/credential-definition/:credential_definition_id', async (req, res) => {
+            const credential_definition_id = req.params.credential_definition_id;
+            try {
+                const result = await this.agentService.getCredDef(credential_definition_id)
+                res.json(result);
+            } catch (error) {
+                console.log(error);
+                res.json(error);
+            }
+        })
+    }
+
+    //#region  Proof Request
+
+    private async sendProofRequest() {
+        this.router.post('/present-proof/send-request', async (req, res) => {
+            const bodyExample = req.body;
+            // const bodyExample = {
+            //     "connection_id": "",
+            //     "proof_request_name": "Proof Of Education",
+            //     "request_attributes": {
+            //         "schema_attrs": ["name", "date", "degree"],
+            //         "restrictions": [{ "credential_definition_id": "WgWxqztrNooG92RXvxSTWv:3:CL:20:tag", "issuer_did": "WgWxqztrNooG92RXvxSTWv" }]
+            //     },
+            //     "requested_predicates": {
+            //         "name": "age",
+            //         "p_value": "18",
+            //         "restrictions": [{
+            //             "issuer_did": "",
+            //             "credential_definition_id": "",
+            //             "cred_def_id": "",
+            //             "schema_id": "",
+            //             "schema_issuer_did": "",
+            //             "schema_name": "",
+            //             "schema_version": "",
+            //         }]
+            //     }
+            // }
+            const attrs = bodyExample.request_attributes.schema_attrs.map(attr => {
+                return {
+                    name: attr,
+                    restrictions: bodyExample.request_attributes.restrictions
+                }
+            })
+            const reqAttrs: IndyProofReqAttrSpec[] = [
+                { "name": "self_attested_thing" },
+                ...attrs
+            ]
+            //zero knowledge proof
+            const reqPreds: IndyProofReqPredSpec[] = [{
+                "name": bodyExample.requested_predicates.name,
+                "p_type": ">=",
+                "p_value": Number(bodyExample.requested_predicates.p_value),
+                "restrictions": bodyExample.requested_predicates.restrictions,
+            }];
+            try {
+                const payload: SendProofRequestPayload = {
+                    requested_attributes: reqAttrs,
+                    requested_predicates: reqPreds,
+                    proof_request_name: bodyExample.proof_request_name
+                }
+                const resp = await this.agentService.buildAndSendProofRequest(bodyExample.connection_id, payload);
+                res.json(resp);
+            } catch (error) {
+                res.json(error);
+            }
+        })
+    }
+
+    //#endregion
 }
